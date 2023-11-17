c51668ecf88e12c645f84c14e654f46f549ae944
kulsoomshaikh1 <kulsoomshaikh71@gmail.com>
2023-11-17 16:00:44 +0500

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,36 +1,23 @@
-// Welcome.js
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import "./styles/Welcome.css";
+import React from 'react'
+import img from './logo.png' 
 
-const Welcome = () => {
+const AboutUs = () => {
   return (
-    <Container style={{ marginTop: '20px' }}>
-      <Row>
-        {/* Description on the left side */}
-        <Col md={6} style={{ textAlign: 'left', padding: '20px' }}>
-        
-          <p>
-            Indulge your senses in the irresistible aroma of freshly baked delights. 
-            At [Your Bakery Name], we take pride in offering a wide range of delectable treats 
-            crafted with passion and the finest ingredients. From mouth-watering pastries to 
-            custom cakes, every bite is a celebration of flavor.
-          </p>
-        </Col>
+    <div className='about-us-container'>
+      <div className='about-us-content'>
+        <h1>About Us</h1>
+        <p>
+        Indulge your senses in a world of delectable delights at our bakery
+        Our passion for baking, combined with the finest ingredients, creates
+         an irresistible array of mouth-watering treats that promise to elevate 
+         your culinary experience.
+        </p>
+      </div>
+      <div className='Welcome-image'>
+        <img src={img} alt='image' />
+      </div>
+    </div>
+  )
+}
 
-        {/* Picture of the bakery on the right side */}
-        <Col md={6} style={{ padding: '20px' }}>
-          <img
-            src="bakery.jpg"  {/* Replace with the actual path to your bakery image */}
-            alt="Bakery Interior"
-            style={{ width: '100%', height: 'auto' }}
-          />
-        </Col>
-      </Row>
-    </Container>
-  );
-};
-
-export default Welcome;
+export default Welcome
