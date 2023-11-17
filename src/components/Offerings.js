a39d4dd2a835fe3df43f190fab99db2b54b9e119
kulsoomshaikh1 <kulsoomshaikh71@gmail.com>
2023-11-17 16:10:01 +0500

    done offerings

--- src/components/Offerings.js
+++ src/components/Offerings.js
@@ -1,82 +1,39 @@
-
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import React from 'react'
+import barfiImage from './barfi1.jfif'
 
 const Offerings = () => {
+  const offeringsData = [
+    { id: 1, name: 'Barfi', price: 'Rs 900', image: barfiImage },
+    { id: 2, name: 'Barfi', price: 'Rs 900', image: barfiImage },
+    { id: 3, name: 'Barfi', price: 'Rs 900', image: barfiImage },
+    { id: 4, name: 'Barfi', price: 'Rs 900', image: barfiImage },
+    { id: 5, name: 'Barfi', price: 'Rs 900', image: barfiImage },
+    { id: 6, name: 'Barfi', price: 'Rs 900', image: barfiImage },
+  ]
+
   return (
-    <Container className="offerings-container mt-4">
-      <h2>Our Offerings</h2>
-
-      {/* First row of offerings */}
-      <Row className="mb-4">
-        <Col md={4}>
-          <img
-            src="path_to_sweet_dish_image_1.jpg"  {/* Replace with the actual path to your sweet dish image */}
-            alt="Sweet Dish 1"
-            className="img-fluid"
-          />
-          <h4>Sweet Dish 1</h4>
-        </Col>
-
-        <Col md={4}>
-          <img
-            src="path_to_sweet_dish_image_2.jpg"  {/* Replace with the actual path to your sweet dish image */}
-            alt="Sweet Dish 2"
-            className="img-fluid"
-          />
-          <h4>Sweet Dish 2</h4>
-        </Col>
-
-        <Col md={4}>
-          <img
-            src="path_to_sweet_dish_image_3.jpg"  {/* Replace with the actual path to your sweet dish image */}
-            alt="Sweet Dish 3"
-            className="img-fluid"
-          />
-          <h4>Sweet Dish 3</h4>
-        </Col>
-      </Row>
-
-      {/* Second row of offerings */}
-      <Row>
-        <Col md={4}>
-          <img
-            src="path_to_sweet_dish_image_4.jpg"  {/* Replace with the actual path to your sweet dish image */}
-            alt="Sweet Dish 4"
-            className="img-fluid"
-          />
-          <h4>Sweet Dish 4</h4>
-          <p>$10.99</p>
-          <Button variant="primary">Order Now</Button>
-        </Col>
-
-        <Col md={4}>
-          <img
-            src="path_to_sweet_dish_image_5.jpg"  {/* Replace with the actual path to your sweet dish image */}
-            alt="Sweet Dish 5"
-            className="img-fluid"
-          />
-          <h4>Sweet Dish 5</h4>
-          <p>$12.99</p>
-          <Button variant="primary">Order Now</Button>
-        </Col>
-
-        <Col md={4}>
-          <img
-            src="path_to_sweet_dish_image_6.jpg"  {/* Replace with the actual path to your sweet dish image */}
-            alt="Sweet Dish 6"
-            className="img-fluid"
-          />
-          <h4>Sweet Dish 6</h4>
-          <p>$8.99</p>
-          <Button variant="primary">Order Now</Button>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+    <div className='offerings-container'>
+      <h1>Our Offerings</h1>
+      <div className='offerings-row'>
+        {offeringsData.slice(0, 3).map((item) => (
+          <div key={item.id} className='offering-item'>
+            <img src={item.image} alt={item.name} />
+            <h3>{item.name}</h3>
+            <p>{item.price}</p>
+          </div>
+        ))}
+      </div>
+      <div className='offerings-row'>
+        {offeringsData.slice(3, 6).map((item) => (
+          <div key={item.id} className='offering-item'>
+            <img src={item.image} alt={item.name} />
+            <h3>{item.name}</h3>
+            <p>{item.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default Offerings;
